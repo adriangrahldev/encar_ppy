710c39ade909d182f00ca6f0f9c232cd233fe5ed
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-05-11 13:51:51 -0400

    refactor: Update weather alert type calculation and API endpoints for department data retrieval

--- server/src/index.js
+++ server/src/index.js
@@ -22,7 +22,7 @@ app.get('/api/departamentos', (req, res) => {
 app.get('/api/departamentos/:id', (req, res) => {
   const departamentoId = req.params.id;
   const departamento = jsonDB.departamento_list.find((departamento) => parseInt(departamento.id) === parseInt(departamentoId));
-  procesarDatos(departamento);
+  procesarDato(departamento);
   res.json(departamento);
 });
 
@@ -40,19 +40,23 @@ const procesarDatos = (departamentos) => {
     });
   });
 };
+const procesarDato = (departamento) => {
+  departamento.pronostico_extendido_list.forEach(pronostico => {
+    pronostico.alerta = getAlertType(pronostico.temperatura_max, pronostico.humedad_max, pronostico.viento_max, pronostico.probabilidad_precipitacion, pronostico.lluvia);
+  });
+};
 
 const getAlertType = (temp, humedad, viento, probabilidad_precipitacion, lluvia) => {
   let alertType = 'optimo';
-  if (temp > 30 && humedad > 80) {
-    alertType = 'peligroso';
-  } else if (temp > 25 && humedad > 70) {
-    alertType = 'precaucion';
-  } else if (viento > 50 || probabilidad_precipitacion > 0.5) {
-    alertType = 'precaucion';
-  } else if (lluvia.volumen_1h > 5) {
-    alertType = 'peligroso';
+
+  if (temp >= 30 || humedad >= 90 || viento >= 30 || probabilidad_precipitacion >= 50 || lluvia >= 10) {
+    alertType = 'peligro';
+  } else if (temp >= 25 || humedad >= 80 || viento >= 20 || probabilidad_precipitacion >= 40 || lluvia >= 5) {
+    alertType = 'advertencia';
   }
+
   return alertType;
+
 };
 
 app.listen(8000, () => {

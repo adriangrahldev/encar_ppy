a774f9e33f80983b68b80e473eff7871f43c04d8
gato513 <antonioprado5606@gmail.com>
2024-05-11 14:41:20 -0400

    estilizado del componente de clima del dia

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,70 +6,79 @@ import { apiUrl } from "./services/config";
 import { WeatherOfTheDay } from "@/components/weatherTfTheDay/WeatherTfTheDay";
 
 export default function Home() {
-  const [searchDepart, setSearchDepart] = useState<string>("");
-  const [searchResults, setSearchResults] = useState([]);
-  const [isFarheneit, setIsFarheneit] = useState<boolean>(false);
+	const [searchDepart, setSearchDepart] = useState<string>("");
+	const [searchResults, setSearchResults] = useState([]);
+	const [isFarheneit, setIsFarheneit] = useState<boolean>(false);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e?.preventDefault();
-    handleSearchDepartment();
-  };
+	const handleSubmit = (e: React.FormEvent) => {
+		e?.preventDefault();
+		handleSearchDepartment();
+	};
 
-  const handleSearchDepartment = async () => {
-    try {
-      const response = await axios.get(
-        `${apiUrl}/departamentos/search/${searchDepart}`
-      );
-      const results = await response.data;
-      setSearchResults(results);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  return (
-    <main>
-      <div className="flex item-center w-full justify-evenly p-4">
-        <div className="w-20">
-         <Image  src="/encar_logo.png" alt="Logo" width="100" height="100" />
-        </div>
-        <form
-          onSubmit={handleSubmit}
-          className="border-2 border-black p-2 w-2/6 flex justify-between"
-        >
-          <input
-            className="focus:outline-none"
-            type="text"
-            placeholder="Buscar ubicación"
-            value={searchDepart}
-            onChange={(e) => setSearchDepart(e.target.value)}
-          />
-          <button className="item-center" type="submit">
-            <Image src="/search.png" alt="Search" width="20" height="20" />
-          </button>
-        </form>
+	const handleSearchDepartment = async () => {
+		try {
+			const response = await axios.get(
+				`${apiUrl}/departamentos/search/${searchDepart}`
+			);
+			const results = await response.data;
+			setSearchResults(results);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	return (
+		<main>
+			<div className="flex item-center w-full justify-evenly p-4">
+				<div className="w-20">
+					<Image
+						src="/encar_logo.png"
+						alt="Logo"
+						width="100"
+						height="100"
+					/>
+				</div>
+				<form
+					onSubmit={handleSubmit}
+					className="border-2 border-black p-2 w-2/6 flex justify-between"
+				>
+					<input
+						className="focus:outline-none"
+						type="text"
+						placeholder="Buscar ubicación"
+						value={searchDepart}
+						onChange={(e) => setSearchDepart(e.target.value)}
+					/>
+					<button className="item-center" type="submit">
+						<Image
+							src="/search.png"
+							alt="Search"
+							width="20"
+							height="20"
+						/>
+					</button>
+				</form>
 
-        <div className="w-1/12 text-center border-black flex border rounded-3xl">
-          <button
-            className={`rounded-l-3xl border-black flex-1 ${
-              !isFarheneit ? "shadow-inner bg-gray-300" : ""
-            }`}
-            onClick={(e) => setIsFarheneit(false)}
-          >
-            C
-          </button>
-          <button
-            className={`rounded-r-3xl border-black flex-1 ${
-              isFarheneit ? "shadow-inner bg-gray-300" : ""
-            }`}
-            onClick={(e) => setIsFarheneit(true)}
-          >
-            F
-          </button>
-        </div>
-      </div>
+				<div className="w-1/12 text-center border-black flex border rounded-3xl">
+					<button
+						className={`rounded-l-3xl border-black flex-1 ${
+							!isFarheneit ? "shadow-inner bg-gray-300" : ""
+						}`}
+						onClick={(e) => setIsFarheneit(false)}
+					>
+						C
+					</button>
+					<button
+						className={`rounded-r-3xl border-black flex-1 ${
+							isFarheneit ? "shadow-inner bg-gray-300" : ""
+						}`}
+						onClick={(e) => setIsFarheneit(true)}
+					>
+						F
+					</button>
+				</div>
+			</div>
 
-      <WeatherOfTheDay city={{hola:"hola"}}/>
-
-    </main>
-  );
+			<WeatherOfTheDay cityWeather={{ hola: "hola" }} />
+		</main>
+	);
 }
--- client/src/components/weatherTfTheDay/WeatherTfTheDay.tsx
+++ client/src/components/weatherTfTheDay/WeatherTfTheDay.tsx
@@ -1,101 +1,55 @@
-"use client"
+"use client";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-const cityWeather = {
-	cod: "200",
-	message: 0,
-	departmentList: [
-		{
-			id: 11,
-			name: "Central",
-			population: 1866562,
-			coordinates: {
-				lat: -25.380646,
-				lon: -57.602323,
-			},
-
-			extendedForecastList: [
-				{
-					dateTimeTxt: "2024-05-11 06:00:00",
-					dayText: "hoy",
-					mainInfo: {
-						temp: 21.34,
-						tempMin: 21.34,
-						tempMax: 30.24,
-						humidity: 100,
-					},
-					weather: [
-						{
-							id: 1,
-							type: "rain",
-							description: "lluvia",
-						},
-					],
-					wind: {
-						speed: 40.05,
-						direction: 66,
-					},
-					visibility: 5000,
-					precipitationProbability: 0.99,
-					rain: {
-						volume1h: 0.4,
-					},
-				},
-
-				// Other forecast objects...
-			],
-		},
-	],
-};
-
 interface Prop {
-	city: object;
+	cityWeather: object;
 }
 
-export const WeatherOfTheDay: React.FC<Prop> = ({ city }) => {
+export const WeatherOfTheDay: React.FC<Prop> = ({ cityWeather }) => {
 
-	const todayForecast = cityWeather.departmentList[0].extendedForecastList[0];
-
-    /* const [todayForecast, setTodayForecast] = useState({})
+	const [todayForecast, setTodayForecast] = useState({})
 
     useEffect(() => {
         setTodayForecast(cityWeather.departmentList[0].extendedForecastList[0];)
-    }, [city]) */
-    
+    }, [cityWeather])
+
 	return (
-		<div >
-			<div>
-				<Image
-					src={"/home-icono.png"}
-					width={50}
-					height={50}
-					alt="home icon"
-				/>
-				<p> - {cityWeather.departmentList[0].name}</p>
-			</div>
+		<div className="flex w-full item-center justify-center">
+			<div
+				className="p-4"
+				style={{ background: "#CFD4FE", borderRadius: 16 }}
+			>
+				<div className="flex gap-2 item-center">
+					<Image
+						src={"/home-icono.png"}
+						width={25}
+						height={25}
+						alt="home icon"
+					/>
+					<p> - Asucion {cityWeather.departmentList[0].name}</p>
+				</div>
 
-			<p>HOY, Sábado </p>
+				<p>HOY, Sábado </p>
 
-			<p>{todayForecast.dateTimeTxt.substring(0, 10)}</p>
+				<p>{todayForecast.dateTimeTxt.substring(0, 10)}</p>
 
-			<h2>{todayForecast.mainInfo.temp}ºC</h2>
+				<h2 style={{ fontSize: "2em" }}>{todayForecast.mainInfo.temp}ºC</h2>
 
-			<h3>Estado del tiempo</h3>
+				<h3>Estado del tiempo</h3>
 
-			<div>
-				<p>Optimo</p>
-			</div>
+				<div>
+					<p>Optimo</p>
+				</div>
 
-			<div>
-				<p>Viento {todayForecast.wind.speed} km/h</p>
-				<p>|</p>
-				<p>Humedad {todayForecast.mainInfo.humidity}%</p>
-				<p>|</p>
-				<p>Precipitación {todayForecast.rain.volume1h} mm/h</p>
+				<div className="flex gap-2">
+					<p>Viento {todayForecast.wind.speed} km/h</p>
+					<p>|</p>
+					<p>Humedad {todayForecast.mainInfo.humidity}%</p>
+					<p>|</p>
+					<p>Precipitación {todayForecast.rain.volume1h} mm/h</p>
+				</div>
 			</div>
 		</div>
 	);
 };
-
-

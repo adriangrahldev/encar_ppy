6b2b485e336dca1b442dfb3379d77045408fcc29
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-05-11 12:52:10 -0400

    Add weather alert type to department data retrieval API endpoints

--- server/src/index.js
+++ server/src/index.js
@@ -7,23 +7,48 @@ const app = express();
 
 app.get('/api/departamentos', (req, res) => {
     const departamentos = jsonDB.departamento_list || [];
-  res.json(departamentos);  
+    procesarDatos(departamentos)
+    res.json(departamentos);  
 });
 
 app.get('/api/departamentos/:id', (req, res) => {
     const departamentoId = req.params.id;    
     const departamento = jsonDB.departamento_list.find((departamento) =>  parseInt(departamento.id) === parseInt(departamentoId) );
+    procesarDatos(departamento)
     res.json(departamento);
 });
 
 app.get('/api/departamentos/search/:name', (req, res) => {
     const departamentoName = req.params.name;
 
-    const departamentos = jsonDB.departamento_list.find((departamento) =>  departamento.nombre.includes(departamentoName) ) || [];
-    
+    const departamentos = jsonDB.departamento_list.filter((departamento) => departamento.nombre.toLowerCase().includes(departamentoName.toLowerCase()));
+    procesarDatos(departamentos);  
     res.json(departamentos);
 });
 
+const procesarDatos = (departamentos) => {
+    departamentos.map(departamento => {
+        departamento.pronostico_extendido_list.map(pronostico => {
+            pronostico.alerta = getAlertType(pronostico.temperatura_max, pronostico.humedad_max, pronostico.viento_max, pronostico.probabilidad_precipitacion, pronostico.lluvia)
+        })
+    });
+}
+
+const getAlertType = (temp, humedad, viento, probabilidad_precipitacion, lluvia) => {
+  let alertType = 'optimo';
+  if (temp > 30 && humedad > 80) {
+    alertType = 'peligroso';
+  } else if (temp > 25 && humedad > 70) {
+    alertType = 'precaucion';
+  } else if (viento > 50) {
+    alertType = 'precaucion';
+  } else if (probabilidad_precipitacion > 0.5) {
+    alertType = 'precaucion';
+  } else if (lluvia.volumen_1h > 5) {
+    alertType = 'peligroso';
+  }
+  return alertType;
+}
 
 app.listen(8000, () => {
   console.log('Server is running on port 3000');

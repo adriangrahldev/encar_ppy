ad82b119714c3a2da05c9420c6b05cb3be58f33c
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2024-05-11 12:05:51 -0400

    Reseteo de estilos

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,24 @@
+import Image from "next/image";
+
 export default function Home() {
-	return <main>hola</main>;
+  return (
+    <main>
+      <div className="flex item-center w-full justify-evenly p-4">
+        <div>
+          <h1>LOGO</h1>
+          <h1>AQUÍ</h1>
+        </div>
+        <div className="border-2 border-black p-2">
+          <input type="text" placeholder="Buscar ubicación" />
+          <button className="item-center w-" type="submit">
+            <Image src="/search.png" alt="Search" width="20" height="20" />
+          </button>
+        </div>
+        <div>
+          <button>C</button>
+          <button>F</button>
+        </div>
+      </div>
+    </main>
+  );
 }
